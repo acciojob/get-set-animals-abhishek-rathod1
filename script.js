d35abf9d8da4b4833446a693c7a1b0c4667fae90
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-04-13 13:52:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species){
+		this.species = species;
+	}
+	get species(){
+		return this.species;
+	}
+	makeSound(){
+		return `The ${species} makes a sound`
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	bark(){
+		return "woof";
+	}
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	purr(){
+		return "purr";
+	}
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
